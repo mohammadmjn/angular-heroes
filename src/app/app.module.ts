568e1d8769093e26496471fdc7e546bc64bb6bb3
mohammadmjn <mojrian70@gmail.com>
2020-08-05 03:35:10 +0430

    Added search by name feature to Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ViewEncapsulation } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDataService } from "./in-memory-data.service";
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
-import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
-import { InMemoryDataService } from "./in-memory-data.service";
 
 @NgModule({
   declarations: [
@@ -19,7 +22,8 @@ import { InMemoryDataService } from "./in-memory-data.service";
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,7 +5,6 @@ import { Observable, of } from "rxjs";
 import { catchError, map, tap } from "rxjs/operators";
 
 import { Hero } from "./hero";
-import { HEROES } from "./mock-heroes";
 import { MessageService } from "./message.service";
 
 @Injectable({
@@ -22,8 +21,33 @@ export class HeroService {
     private messageService: MessageService,
     private http: HttpClient) { }
 
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.log(error);
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T)
+    };
+  }
+
+  getHeroNo404<Data>(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/?id=${id}`;
+    return this.http.get<Hero[]>(url).pipe(
+      map(heroes => heroes[0]),
+      tap(h => {
+        const outcome = h ? `fetched` : `did not find`;
+        this.log(`${outcome} hero id=${id}`);
+      }),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
   getHeroes(): Observable<Hero[]> {
-    this.log('fetched heroes');
     return this.http.get<Hero[]>(this.heroesUrl)
       .pipe(
         tap(_ => this.log('fetched heroes')),
@@ -39,32 +63,10 @@ export class HeroService {
     );
   }
 
-  private log(message: string) {
-    this.messageService.add(`HeroService: ${message}`);
-  }
-
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      // TODO: send the error to remote logging infrastructure
-      console.log(error);
-      // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
-      return of(result as T)
-    };
-  }
-
-  updateHero(hero: Hero): Observable<any> {
-    return this.http.put(this.heroesUrl, hero, this.httpOptions)
-      .pipe(
-        tap(_ => this.log(`updated hero id = ${hero.id}`)),
-        catchError(this.handleError<any>('updateHero'))
-      );
-  }
-
   addHero(hero: Hero): Observable<Hero> {
     return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions)
       .pipe(
-        tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+        tap((newHero: Hero) => this.log(`added hero id=${newHero.id} name=${newHero.name}`)),
         catchError(this.handleError<Hero>('addHero'))
       );
   }
@@ -76,6 +78,29 @@ export class HeroService {
     return this.http.delete<Hero>(url, this.httpOptions).pipe(
       tap(_ => this.log(`deleted hero id=${id}`)),
       catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions)
+      .pipe(
+        tap(_ => this.log(`updated hero id = ${hero.id}`)),
+        catchError(this.handleError<any>('updateHero'))
+      );
+  }
+
+  searchHeroes(query: string): Observable<Hero[]> {
+    if (!query.trim()) {
+      return of([]);
+    }
+
+    var url = `${this.heroesUrl}/?name=${query.trim()}`
+    return this.http.get<Hero[]>(url).pipe(
+      tap(x => x.length ?
+        this.log(`found heroes matching "${query}"`) :
+        this.log(`no heroes matching "${query}"`)
+      ),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
     );
   }
 }
