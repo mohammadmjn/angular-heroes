3fac050a29de9c0043b841f5da3e1b16b1291691
mohammadmjn <mojrian70@gmail.com>
2020-08-04 23:37:25 +0430

    Added "Add" and "Delete" heroes feature

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -21,4 +21,17 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes()
       .subscribe(subs_heroes => this.heroes = subs_heroes)
   }
+
+  add(name: string): void {
+    name = name.trim();
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(current_hero => (current_hero !== hero))
+    this.heroService.deleteHero(hero).subscribe();
+  }
 }
